6959920d82a93b02d5c10dc7bca328020c0eb8e0
yuyizhong <>
2023-06-05 14:51:46 +0000

    removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,13 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#x*jnzd1(^&r6#yguzuf@e+&32pcfrs1=4p(v4rhcyb6(*nj2a')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
 if development:
-    ALLOWED_HOSTS = ['https://8000-yuyizhong-ci-fsf-hello-d-kfgdnt57j8.us2.codeanyapp.com']
+    ALLOWED_HOSTS = [
+        '8000-yuyizhong-ci-fsf-hello-d-kfgdnt57j8.us2.codeanyapp.com']
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
@@ -95,7 +96,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
 
 
 # Password validation
